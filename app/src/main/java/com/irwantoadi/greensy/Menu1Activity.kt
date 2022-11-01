5855468c4ed957e3280569b097698a7c27c02cfc
irwantoadi <irwantoadi926@gmail.com>
2022-11-01 22:07:00 +0700

    Project Desain Prototype Aplikasi Greensy

---
+++ app/src/main/java/com/irwantoadi/greensy/Menu1Activity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.greensy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu1)
+    }
+}
---
+++ app/src/main/java/com/irwantoadi/greensy/Menu6Activity.kt
@@ -0,0 +1,11 @@
+package com.irwantoadi.greensy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Menu6Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu6)
+    }
+}
